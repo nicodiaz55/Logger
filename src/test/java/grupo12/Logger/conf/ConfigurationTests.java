ac20e968bd27d392d835b99b7e80d7e7bd0aba42
nicodiaz55 <nicolasdiaz55@yahoo.com.ar>
2014-05-23 00:03:32 -0300

    Más test para Configuration

--- src/test/java/grupo12/Logger/conf/ConfigurationTests.java
+++ src/test/java/grupo12/Logger/conf/ConfigurationTests.java
@@ -34,42 +34,6 @@ public class ConfigurationTests {
 		assertEquals(1, strings.size());
 		assertTrue(strings.contains(Configuration.defaultOutput));
 	}
-	
-	@Test
-	// Tests a multiple configuration (multiple outputs, patterns, levels and separators)
-	public void customConfiguration() {
-		// Setup:
-		Configuration conf = new Configuration("user.cfg");
-		List<String> strings;
-		
-		// Check levels:
-		strings = conf.getLevels();
-		assertEquals(3, strings.size());
-		assertEquals("INFO",strings.get(0));
-		assertEquals("DEBUG",strings.get(1));
-		assertEquals("FATAL",strings.get(2));
-		
-		// Check message formats:
-		strings = conf.getPatterns();
-		assertEquals(3, strings.size());
-		assertEquals("%d{HH:mm:ss} %n %m", strings.get(0));
-		assertEquals("%d{HH:mm:ss} %n %p %n %m", strings.get(1));
-		assertEquals("%d{HH:mm:ss} %n %t %n %m", strings.get(2));
-		
-		// Check separators:
-		strings = conf.getSeparators();
-		assertEquals(3, strings.size());
-		assertEquals("|", strings.get(0));
-		assertEquals("-", strings.get(1));
-		assertEquals("/", strings.get(2));
-		
-		// Check outputs:
-		strings = conf.getOutputs();
-		assertEquals(3, strings.size());
-		assertEquals("log1.txt", strings.get(0));
-		assertEquals("log2.txt", strings.get(1));
-		assertEquals("console", strings.get(2));
-	}
 
 	@Test
 	public void loadFileWithoutLevelInfo() {
@@ -117,5 +81,99 @@ public class ConfigurationTests {
 		strings = conf.getSeparators();
 		assertEquals(1, strings.size());
 		assertTrue(strings.contains(Configuration.defaultSeparator));
+	}
+	
+	@Test
+	public void loadFileWithMultipleLevelInfo() {
+		// Setup:
+		Configuration conf = new Configuration("multipleLevels.cfg");
+		List<String> strings;
+		
+		// Check level:
+		strings = conf.getLevels();
+		assertEquals(3, strings.size());
+		assertEquals("INFO",strings.get(0));
+		assertEquals("DEBUG",strings.get(1));
+		assertEquals("FATAL",strings.get(2));
+	}
+	
+	@Test
+	public void loadFileWithMultipleOutputsInfo() {
+		// Setup:
+		Configuration conf = new Configuration("multipleOutputs.cfg");
+		List<String> strings;
+		
+		// Check level:
+		strings = conf.getOutputs();
+		assertEquals(3, strings.size());
+		assertEquals("log1.txt", strings.get(0));
+		assertEquals("log2.txt", strings.get(1));
+		assertEquals("console", strings.get(2));
+	}
+	
+	@Test
+	public void loadFileWithMultiplePatternsInfo() {
+		// Setup:
+		Configuration conf = new Configuration("multiplePatterns.cfg");
+		List<String> strings;
+		
+		// Check level:
+		strings = conf.getPatterns();
+		assertEquals(3, strings.size());
+		assertEquals("%d{HH:mm:ss} %n %m", strings.get(0));
+		assertEquals("%d{HH:mm:ss} %n %p %n %m", strings.get(1));
+		assertEquals("%d{HH:mm:ss} %n %t %n %m", strings.get(2));
+	}
+	
+	@Test
+	public void loadFileWithMultipleSeparatorInfo() {
+		// Setup:
+		Configuration conf = new Configuration("multipleSeparators.cfg");
+		List<String> strings;
+		
+		// Check level:
+		strings = conf.getSeparators();
+		assertEquals(3, strings.size());
+		assertEquals("|", strings.get(0));
+		assertEquals("-", strings.get(1));
+		assertEquals("/", strings.get(2));
+	}
+	
+	
+	@Test
+	// Integration Test.
+	// Tests a multiple configuration (multiple outputs, patterns, levels and separators)
+	public void customConfiguration() {
+		// Setup:
+		Configuration conf = new Configuration("user.cfg");
+		List<String> strings;
+		
+		// Check levels:
+		strings = conf.getLevels();
+		assertEquals(3, strings.size());
+		assertEquals("INFO",strings.get(0));
+		assertEquals("DEBUG",strings.get(1));
+		assertEquals("FATAL",strings.get(2));
+		
+		// Check message formats:
+		strings = conf.getPatterns();
+		assertEquals(3, strings.size());
+		assertEquals("%d{HH:mm:ss} %n %m", strings.get(0));
+		assertEquals("%d{HH:mm:ss} %n %p %n %m", strings.get(1));
+		assertEquals("%d{HH:mm:ss} %n %t %n %m", strings.get(2));
+		
+		// Check separators:
+		strings = conf.getSeparators();
+		assertEquals(3, strings.size());
+		assertEquals("|", strings.get(0));
+		assertEquals("-", strings.get(1));
+		assertEquals("/", strings.get(2));
+		
+		// Check outputs:
+		strings = conf.getOutputs();
+		assertEquals(3, strings.size());
+		assertEquals("log1.txt", strings.get(0));
+		assertEquals("log2.txt", strings.get(1));
+		assertEquals("console", strings.get(2));
 	}
 }
