b686ee126a1a44a2b7e79b476e43481cb0f3123e
alex <alexing10@gmail.com>
2014-06-04 16:04:28 -0300

    arreglado para que el xml se maneje con una lista de Configurations

--- src/main/java/grupo12/Logger/conf/XMLReader.java
+++ src/main/java/grupo12/Logger/conf/XMLReader.java
@@ -14,51 +14,47 @@ import org.xml.sax.SAXParseException;
 
 public class XMLReader {
 
-	private List<User> userList = new ArrayList<User>();
+	private List<Configuration> confList = new ArrayList<Configuration>();
 	
-	public XMLReader() {
-		//TODO: este path es muy feo
-		this("src/main/resources/default.xml");
-	}
 	
 	public XMLReader(String configurationXML){
 		try {
 			DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-			Document doc = docBuilder.parse (new File(configurationXML));
+			Document doc = docBuilder.parse (new File("src/main/resources/"+configurationXML));
         
 			doc.getDocumentElement ().normalize ();
 			NodeList listOfUsers = doc.getElementsByTagName("user");
 			int totalUsers = listOfUsers.getLength();
         
 			for(int s=0; s<totalUsers ; s++){
-				Node userNode = listOfUsers.item(s);
-                if(userNode.getNodeType() == Node.ELEMENT_NODE){
+				Node confNode = listOfUsers.item(s);
+                if(confNode.getNodeType() == Node.ELEMENT_NODE){
                 	
-                	Element userElement = (Element)userNode;
-                	User aUser=new User();
+                	Element confElement = (Element)confNode;
+                	Configuration aConfiguration=new Configuration();
                 	
-                	NodeList userNameList = userElement.getElementsByTagName("userName");
+                	NodeList userNameList = confElement.getElementsByTagName("userName");
                     Element userNameElement = (Element)userNameList.item(0);
                     NodeList textUserNameList = userNameElement.getChildNodes();
-                    aUser.setUserName(((Node)textUserNameList.item(0)).getNodeValue().trim());
+                    aConfiguration.setName(((Node)textUserNameList.item(0)).getNodeValue().trim());
                     
-                    NodeList modeList = userElement.getElementsByTagName("mode");
+                    NodeList modeList = confElement.getElementsByTagName("mode");
                     Element modeElement = (Element)modeList.item(0);
                     NodeList textModeList = modeElement.getChildNodes();
-                    aUser.setMode(((Node)textModeList.item(0)).getNodeValue().trim());
+                    aConfiguration.setLevels(((Node)textModeList.item(0)).getNodeValue().trim());
                     
-                    NodeList patternList = userElement.getElementsByTagName("pattern");
+                    NodeList patternList = confElement.getElementsByTagName("pattern");
                     Element patternElement = (Element)patternList.item(0);
                     NodeList textPatternList = patternElement.getChildNodes();
-                    aUser.setPattern(((Node)textPatternList.item(0)).getNodeValue().trim());
+                    aConfiguration.setFormatters(((Node)textPatternList.item(0)).getNodeValue().trim());
                     
-                    NodeList outputList = userElement.getElementsByTagName("output");
+                    NodeList outputList = confElement.getElementsByTagName("output");
                     Element outputElement = (Element)outputList.item(0);
                     NodeList textOutputList = outputElement.getChildNodes();
-                    aUser.setOutput(((Node)textOutputList.item(0)).getNodeValue().trim());
+                    aConfiguration.setOutputs(((Node)textOutputList.item(0)).getNodeValue().trim());
                     
-                    userList.add(aUser);
+                    confList.add(aConfiguration);
                 }
         	
 			}
@@ -76,18 +72,18 @@ public class XMLReader {
 	           t.printStackTrace ();
 	           }
 	}
-	public List<User> getUserList(){
-		return userList;
+	public List<Configuration> getConfigurationsList(){
+		return confList;
 	}
 	
-//	public static void main (String argv []){
-//		XMLReader XMLprueba=new XMLReader();
-//		for (User u : XMLprueba.getUserList()){
-//			System.out.println(u.getUserName());
-//			System.out.println(u.getMode());
-//			System.out.println(u.getPattern());
-//			System.out.println(u.getOutput());
-//		}
-//	}
+	public static void main (String argv []){
+		XMLReader XMLprueba=new XMLReader("default.xml");
+		for (Configuration c : XMLprueba.getConfigurationsList()){
+			System.out.println(c.getName());
+			System.out.println(c.getLevels());
+			System.out.println(c.getFormatters());
+			System.out.println(c.getOutputs());
+		}
+	}
 }
 	
