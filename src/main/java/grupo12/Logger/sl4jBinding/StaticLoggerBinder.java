260d3ccc79e0a08abe3a0a438db47c01e67f7962
Joaquin <joaquinstan@gmail.com>
2014-06-05 17:37:01 -0300

    Un pequenio error de nombre en StaticLoggerBinder

--- src/main/java/grupo12/Logger/sl4jBinding/StaticLoggerBinder.java
+++ src/main/java/grupo12/Logger/sl4jBinding/StaticLoggerBinder.java
@@ -29,7 +29,7 @@ public class StaticLoggerBinder implements LoggerFactoryBinder {
 	// to avoid constant folding by the compiler, this field must *not* be final
 	public static String REQUESTED_API_VERSION = "1.7.7"; // !final
 
-	private static final String loggerFactoryClassStr = LoggerFactory.class
+	private static final String loggerFactoryClassStr = AdapterFactory.class
 			.getName();
 
 	/**
